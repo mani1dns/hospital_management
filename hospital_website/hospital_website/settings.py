b5a2ee05db108b13728f001755601ba3171dbe0f
Mani A <mani.msd1357@gmail.com>
2023-08-24 12:00:31 +0530

    TypeError: argument of type WindowsPath is not iterable & NoneType object is not subscriptable is solved

--- hospital_website/hospital_website/settings.py
+++ hospital_website/hospital_website/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'hospital_website.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3")),
     }
 }
 
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
